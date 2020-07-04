cdd222cde1bc4b9ea2f1bcc452872560011ac815
wukimwa <53428658+wukimwa@users.noreply.github.com>
2020-07-04 14:42:51 +0800

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -3,12 +3,40 @@ using namespace std;
 
 // function header
 void menu();
+void player();
+void dungeon();
+void dungeonNormal();
 
 // global variable
 string playerName;
 int playerLv = 0;
 int playerExp = 0;
 int playerCoin = 0;
+
+
+
+void menu() {
+    int number = 0;
+
+    do {
+
+        cout << endl << endl;
+        cout << "---Main Menu---" << endl;;
+        cout << "1) Player" << endl;
+        cout << "2) Dungeon" << endl;
+        cout << "3) Exit Game" << endl;
+        cin >> number;
+        cout << endl;
+
+        switch (number) {
+        case 1: player(); break;
+        case 2: dungeon(); break;
+        case 3: break;
+        }
+
+    } while (number >= 1 && number <= 2);
+
+}
 
 void player() {
     int number = 0;
@@ -33,29 +61,40 @@ void player() {
 }
 
 void dungeon() {
-    cout << "Dungeon Developing";
-}
-
-void menu() {
     int number = 0;
 
     do {
 
         cout << endl << endl;
-        cout << "---Main Menu---" << endl;;
-        cout << "1) Player" << endl;
-        cout << "2) Dungeon" << endl;
-        cout << "3) Exit Game" << endl;
+        cout << "---Dungeon Menu---" << endl;
+        cout << "1) Go into the dungeon" << endl;
+        cout << "2) Exit To Menu" << endl;
         cin >> number;
         cout << endl;
 
         switch (number) {
-        case 1: player(); break;
-        case 2: dungeon(); break;
-        case 3: break;
+        case 1: dungeonNormal(); break;
+        case 2: menu(); break;
         }
 
-    } while (number >= 1 && number <= 2);
+    } while (number >= 1 && number <= 3);
+}
+
+void dungeonNormal() {
+    int number = 0;
+    int attack;
+    cout << "Press enter to attack the monster." << endl;
+
+    do {
+        cout << "Attacking...";
+        cin.ignore();
+        number++;
+    } while (number < 25);
+
+    if (number >= 25) {
+        cout << "Dungeon Completed" << endl;
+        menu();
+    }
 
 }
 
