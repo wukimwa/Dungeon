3580bbcd1a06626f5dac6a69c9f921ce37811a3c
wukimwa <53428658+wukimwa@users.noreply.github.com>
2020-07-15 15:58:39 +0800

    v1.5.1.7

--- code/menu.cpp
+++ code/menu.cpp
@@ -6,13 +6,13 @@ void showMenu(string m){
     if(m == "Player Menu"){
         cout << "1) Weapon Store" << endl;
         cout << "2) Level Up Rewards" << endl;
-        cout << "3) Exit To Menu" << endl;
+        cout << "3) Exit To Menu" << endl << endl;
     }
     else if(m == "Dungeon Menu"){
         cout << "1) ENTER Dungeon  [ Easy ]" << endl;
         cout << "2) ENTER Dungeon  [Normal]" << endl;
         cout << "3) ENTER Dungeon  " << "[ Hard ] [Lv.05 Unlock]" << endl;
         cout << "4) ENTER Dungeon  " << "[Mythic] [Lv.15 Unlock]" << endl;
-        cout << "5) Exit To Menu" << endl;
+        cout << "5) Exit To Menu" << endl << endl;
     }
 }
